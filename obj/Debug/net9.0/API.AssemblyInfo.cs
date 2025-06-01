047c2eac3a0583ae439d411bbdc40646b4976bdc
Ermal1 <ermal_aliu10@hotmail.com>
2025-06-02 00:14:33 +0200

    chages

--- obj/Debug/net9.0/API.AssemblyInfo.cs
+++ obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c21addef3ec8af291b032ef0f76b0561a9e156ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d6dae9a057234bdebd4895b5b0d1521dde524fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
